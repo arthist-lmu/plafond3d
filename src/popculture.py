16e4f56ef9dcff8064068701bfe2d949f3187724
kristbaum <max@kristenonline.de>
2022-07-11 11:45:52 +0200

    add lxml parser

--- src/popculture.py
+++ src/popculture.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import os
+from os.path import exists
 import time
 
 
@@ -31,20 +32,36 @@ def extract_contents(r):
 
 
 for x in range(1, 2):
-    url = "https://www.pop.culture.gouv.fr/search/list?resPage=" + str(x) + "&periode=%5B%2217e+si%C3%A8cle%22%2C%2216e+si%C3%A8cle%22%5D&type=%5B%22plafond%22%5D"
-    print(url)
-    #r = requests.get(url, timeout=(3, 27))
-    os.environ["TMPDIR"] = "tmp"
-    
-    options = Options()
-    options.add_argument("--headless")
-    browser = webdriver.Firefox(options=options, executable_path=r'./geckodriver')
-    browser.get(url)
-    html = browser.page_source
-    time.sleep(2)
-    print(html)
+    if not exists("cache.html"):
+        url = "https://www.pop.culture.gouv.fr/search/list?resPage=" + \
+            str(x) + "&periode=%5B%2217e+si%C3%A8cle%22%2C%2216e+si%C3%A8cle%22%5D&type=%5B%22plafond%22%5D"
+        print(url)
+        #r = requests.get(url, timeout=(3, 27))
+        os.environ["TMPDIR"] = "tmp"
 
-    # close web browser
-    browser.close()
-    #extract_contents(r)
-    #soup = BeautifulSoup(html, 'lxml')
+        options = Options()
+        options.add_argument("--headless")
+        browser = webdriver.Firefox(
+            options=options, executable_path=r'./geckodriver')
+        browser.get(url)
+        time.sleep(2)
+
+        html = browser.page_source
+        browser.close()
+
+        cache_file = open("cache.html", "w")
+        cache_file.write(html)
+        cache_file.close()
+
+    else:
+        cache_file = open("cache.html", "r")
+        html = cache_file.read()
+        print("Size of site: " + str(len(html)))
+        # close web browser
+        soup = BeautifulSoup(html, 'lxml')
+        #content = soup.find_all('list-card-container')
+        results = soup.find('div', class_='react-es-results').prettify()
+        print("Size of site: " + str(len(results)))
+
+#        print(results)
+        # extract_contents(r)
